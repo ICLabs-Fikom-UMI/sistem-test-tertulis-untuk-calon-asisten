c761d1d67f347a6ce699ebb287bbb79c373fde97
Annisa pratama putri <127636274+Annisapratama11@users.noreply.github.com>
2024-02-02 23:47:48 +0800

    Delete app directory

--- app/views/templates/script.js
+++
@@ -1,8 +0,0 @@
-
-$(".menu > ul > li").click(function (e) {
-    $(this).sibilings().removeClass("active")
-    $(this).toggleClass("active");
-    $(this).find("ul").sideToggle();
-    $(this).sibilings().find("ul").slideUp();
-    4(this).sibilinga().find("ul").find("li").removeClass("active");
-});
