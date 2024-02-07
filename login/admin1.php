480ecdb6df47e2ac43ac7830a9d265f910ed9dd9
Annisa pratama putri <127636274+Annisapratama11@users.noreply.github.com>
2024-02-07 08:45:58 +0800

    Add files via upload

---
+++ login/admin1.php
@@ -0,0 +1,8 @@
+<?php
+include("header.php");
+?>
+<h1>Halaman Admin</h1>
+Selamat datang di halaman admin
+<?php
+include("footer.php");
+?>
---
+++ login/calon_asisten.php
@@ -0,0 +1,8 @@
+<?php
+include("header.php");
+?>
+<h1>Halaman Calon_asisten</h1>
+Selamat datang di halaman user
+<?php
+include("footer.php");
+?>
